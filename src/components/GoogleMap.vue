cf85cd9724cbe924ce65809327c9d3db4ec6302f
DanielCarrascoPallaleo <carrascopallaleo.daniel@gmail.com>
2024-05-10 22:38:49 -0400

    componente googlemaps

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+const center = { lat: 40.689247, lng: -74.044502 }
+</script>
+
+<template>
+  <GoogleMap
+  api-key="AIzaSyDP6D15S_cbaSo4zRlWfZexneA3g1SEp78"
+  style="width: 100%; height: 500px"
+  :center="center"
+  :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
