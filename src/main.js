0209bc0929ef04ede178573b9c90ea9834b5a6b9
JeysonFernandezF <82329309+JeysonFernandezF@users.noreply.github.com>
2024-06-02 19:22:10 -0400

    ADD: Login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+
 import router from './router'
 if (!navigator.geolocation) {
     alert('tu navegador no soporta geolocation');
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#CC4D28',
+        light:'#FF621F',
+        secondary:'#D9D9D9',
+        gray:'#595959',
+        black:'#000000'
+      }
+    },
+  },
+  plugins: [],
+}
